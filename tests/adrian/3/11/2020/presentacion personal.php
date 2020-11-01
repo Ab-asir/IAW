92de62bf19fb1928030313c0386c39f6fdbbbaf6
Ab-asir <adrianburgooss@gmail.com>
2020-11-01 18:22:04 +0100

    presentacion

---
+++ tests/adrian/3/11/2020/presentacion personal.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Presentacion personal</title>
+</head>
+
+<body>
+  <?php
+  $nombre = "Adrian";
+  $apellidos = "Burgos Sastre"
+  $edad = "19 años"
+  echo "Me llamo $nombre $apellidos"
+  ?>
+</body>
+
+</html>
