1e2efb40f061164ed3497236e1bf266b7352e3a9
Ab-asir <adrianburgooss@gmail.com>
2020-11-05 13:50:23 +0100

    funciones php

--- tests/adrian/5-11-2020/funciones.php
+++ tests/adrian/5-11-2020/funciones.php
@@ -1,28 +1,30 @@
 <?php
- function openHtmlTag() {
-     echo "<html>";
- }
- function closeHtmlmTag() {
-     echo "</html>";
- }
- function opentag($tag, $id, $class = "") {
-     echo "<".$tag." id= ".$id." class=".$class.">"
-
- }
- function closetag($tag) {
-     echo "</".tag.">";
- }
- function sayWhatever($whatever) {
-     echo "$whatever";
- }
- openHtmltag();
- opentag("title");
- sayWhatever("Este es el titulo");
- closetag("title");
- opentag("h1", "class=red");
- sayWhatever("Este es el titulo")
- closetag(h1);
- opentag("p", "class=red");
- sayWhatever("Hola este es el contenido");
- closetag(p);
- closeHtmlmTag();
+function openHtmlTag(){
+	echo "<html>";
+}
+function closeHtmlTag(){
+	echo "</html>";
+}
+function openTag($tag, $id, $class = ""){
+	echo "<".$tag." class=".$class.">";
+}
+function closeTag($tag){
+	echo "</".$tag.">";
+}
+function titulo(){
+	echo "Este es el titulo";
+}
+function sayWhatever($whatever){
+	echo "$whatever";
+}
+openHtmlTag();
+opentag("title", "class=red");
+titulo();
+closetag("title");
+opentag("h1", "class=red");
+sayWhatever("Este es el titulo");
+closetag("h1");
+opentag("p", "class=red");
+sayWhatever("Hola Este es el contenido");
+closeTag("p");
+closeHtmlTag();
