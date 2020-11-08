d7653ad97ea8d772b6ad90c365725d27a07771b7
Ab-asir <adrianburgooss@gmail.com>
2020-11-08 18:19:41 +0100

    Lenguaje del servidor - php

---
+++ tests/adrian/10-11-2020/practicas/p-iaw-1/ejercicio2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Programa para multiplicar</title>
+    </head>
+    <body>
+        <?php
+            $tabla = 1;
+            echo "La tabla del $tabla <br>";
+            for($nº=1; $nº<=10; $nº++){
+            echo "$tabla x $nº  = ". ($tabla*$nº) ." <br>";
+            };
+        ?> 
+    </body>
+</html>
